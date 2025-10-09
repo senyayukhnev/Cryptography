6d41203c4f6b9a02a00866be006e3cd5cb81b80f
Алсений <yukhnya@gmail.com>
2025-10-09 20:26:46 +0300

    hello world

---
+++ bits_permutation/cmake-build-debug/CMakeFiles/bits_permutation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bits_permutation.dir/main.cpp.o"
+  "CMakeFiles/bits_permutation.dir/main.cpp.o.d"
+  "bits_permutation"
+  "bits_permutation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bits_permutation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
